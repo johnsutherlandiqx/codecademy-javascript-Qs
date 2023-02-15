4e2d5065246c79be861fb157a12992dc0ccdfc54
johnsutherlandiqx <123561236+johnsutherlandiqx@users.noreply.github.com>
2023-02-15 17:03:34 +0000

    Update qs.js

--- qs.js
+++ qs.js
@@ -28,3 +28,57 @@ if (walkSignal === 'Walk') {
 } else {   
     console.log('Do not walk!');
 }
+
+//answer
+walkSignal === 'Walk' ? console.log('You may walk!') : console.log('Do not walk!');
+
+//3. Which of the following variables contains a truthy value?
+
+
+let varFour = '';
+
+let varThree = 0;
+
+let varOne = 'false';
+
+let varTwo = false;
+
+//answer - 
+let varOne = 'false';
+
+//4. To make this statement valid, what operator belongs in the ___ space below?
+
+if (coin ___ "heads") {
+  console.log('coin is heads!');
+} else {
+  console.log('coin is tails...');
+}
+
+:=
+either = or ===
+=
+===
+ 
+ //answer - === compares values
+ 
+ 5. What will the code block log to the console?
+
+let runTime = 35;
+let runDistance = 3.5;
+ 
+if (runTime <= 30 && runDistance > 3.5) {
+  console.log("You're super fast!");
+} else if (runTime >= 30 && runDistance <= 3) {
+  console.log("You're not making your pace!");
+} else if (runTime > 30 || runDistance > 3) {
+  console.log("Nice workout!"); 
+} else {
+  console.log("Keep on running!");
+}
+
+You’re not making your pace!
+Keep on running!
+Nice workout!
+You’re super fast!
+ 
+ //answer - 
